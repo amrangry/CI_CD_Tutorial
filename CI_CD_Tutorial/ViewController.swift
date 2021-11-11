775af28b969e83a40b62ad10fa5adbcbadc73adb
AmrAngry <Amr.Elghadban@seddiqiholding.com>
2021-11-11 11:01:03 +0400

    init project files

---
+++ CI_CD_Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CI_CD_Tutorial
+//
+//  Created by iSeddiqi Apple on 10/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
